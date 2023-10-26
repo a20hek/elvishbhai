43fdfa41f10f6db5802919d390beb88246493661
abhishek <abbhishekk27@gmail.com>
2023-10-26 18:30:54 +0530

    added script taunts

--- src/lib/mixpanel.ts
+++ src/lib/mixpanel.ts
@@ -4,15 +4,12 @@ mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_KEY as string);
 
 export const Mixpanel = {
 	trackVote: (vote: 'Y' | 'N') => {
-		console.log(`Tracking vote: ${vote}`);
 		mixpanel.track('Vote', { vote });
 	},
 	trackVisitor: () => {
-		console.log('Tracking new visitor');
 		mixpanel.track('New Visitor');
 	},
 	trackMessageSent: () => {
-		console.log('Tracking message sent');
 		mixpanel.track('Message Sent');
 	},
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,53 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
+
+const initMessage = () => {
+	console.log(
+		'%celvish bhai sees you.',
+		'color: light-green; font-size: 22px; font-weight: medium;'
+	);
+};
+
+const consoleOpenMsg = () => {
+	console.log(
+		'%celvish bhai wants you to get a life.',
+		'color: red; font-size: 24px; font-weight: medium;'
+	);
+};
 
 export default function App({ Component, pageProps }: AppProps) {
+	const [messageDisplayed, setMessageDisplayed] = useState(false);
+	const [initMessageDisplayed, setInitMessageDisplayed] = useState(false);
+	useEffect(() => {
+		if (!initMessageDisplayed) {
+			initMessage();
+			setInitMessageDisplayed(true);
+		}
+
+		let timeoutId: NodeJS.Timeout;
+
+		const onConsoleOpen = () => {
+			if (timeoutId) {
+				clearTimeout(timeoutId);
+			}
+
+			timeoutId = setTimeout(() => {
+				if (!messageDisplayed) {
+					consoleOpenMsg();
+					setMessageDisplayed(true);
+				}
+			}, 5000);
+		};
+
+		window.addEventListener('resize', onConsoleOpen);
+
+		return () => {
+			window.removeEventListener('resize', onConsoleOpen);
+			if (timeoutId) {
+				clearTimeout(timeoutId);
+			}
+		};
+	}, [messageDisplayed, initMessageDisplayed]);
 	return <Component {...pageProps} />;
 }
